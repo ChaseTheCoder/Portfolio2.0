a597f5dfe1c6bd7b1b147d0552bb63a4ff7fb589
ChaseTheCoder <chasefsheaff@gmail.com>
2020-10-22 20:55:51 -0500

    modified Home to App, Modified constructor, files for Components added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Chase Sheaff',
+            headerLinks: [
+                { title: 'Home', path: '/' },
+                { title: 'About', path: '/about' },
+                { title: 'Projects', path: '/projects' },
+                { title: 'Resume', path: '/resume' },
+                { title: 'Contact', path: '/contact ' }
+            ],
+            home: {
+                title: 'Soft and Hard Skills To Bring Success',
+                subTitle: 'A teacher with tenacity to get projects done',
+                text: 'Lorme ipsum lorme san sinsu isaca'
+            },
+            about: {
+                title: 'About me',
+                subTitle: 'Software Engineer with an education and tech startup background. I’ve learned to be an expert working cross-functionally on many projects with successful outcomes in education and tech.  My goal is to make intuitive applications and develop along with an inclusive company.'
+            },
+            Projects: {
+                title: 'Resume',
+            },
+            Resume: {
+                title: 'Resume',
+            },
+            Contact: {
+                title: 'Let\'s chat!',
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <h2>Hey Gurl</h2>
+            </Router>
+      );
+    }
 }
 
 export default App;
