0458d8f7e30fe178230d4dda779002c035bbf5f9
ChaseTheCoder <chasefsheaff@gmail.com>
2020-10-24 22:00:47 -0500

    modified Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -56,6 +56,22 @@ class Carousel extends React.Component {
     }
   }
 
+  handleCardClick = (id, card) => {
+    let items = [...this.state.items];
+
+    items[id].selected = items[id].selected ? false : true;
+
+    items.forEach(item => {
+      if(item.ed !== id) {
+        item.selected = false;
+      } 
+    });
+
+    this.setState({
+      items
+    });
+  }
+
   render() {
     return(
       <p>Carousle, Hey</p> 
